d43f2a3c947cfc57237c48eaa00600e7d2678f5f
Vasilis Nicolaou <vaslabs@yahoo.gr>
2017-09-26 23:31:44 +0100

    Cleanup

--- src/main/java/org/vaslabs/github/lombok/DummyObject.java
+++ src/main/java/org/vaslabs/github/lombok/DummyObject.java
@@ -1,6 +1,5 @@
 package org.vaslabs.github.lombok;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.google.common.collect.ImmutableList;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
